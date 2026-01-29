08671e7adba873cdfb87a8ebf5a310c43c57b31a
Fiercedarkvenom <db220605@gmail.com>
2026-01-28 22:48:19 -0800

    Worked UserEntity for the users table

---
+++ app/src/main/java/com/example/cst438_project_1/data/db/UserEntity.kt
@@ -0,0 +1,22 @@
+package com.example.cst438_project_1.data.db
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "users",
+    indices = [Index(value = ["username"], unique = true)]
+)
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val username: String,
+
+    // Stored as BLOB via TypeConverters
+    val salt: ByteArray,
+    val passwordHash: ByteArray,
+
+    val createdAt: Long = System.currentTimeMillis(),
+    val highestStreak: Int = 0,
+    val currentStreak: Int = 0
+)
