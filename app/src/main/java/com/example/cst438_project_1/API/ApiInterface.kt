3f337066cbdfdac91511f3c437466853dbda26ad
ayaayethan <ayaayethan@gmail.com>
2026-01-28 17:56:23 -0800

    created ApiInterface and Game data class

---
+++ app/src/main/java/com/example/cst438_project_1/API/ApiInterface.kt
@@ -0,0 +1,11 @@
+package com.example.cst438_project_1.API
+
+import com.example.cst438_project_1.API.DataClass.Game
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiInterface {
+    @GET("/games")
+    fun getGames(@Query("key") key : String): Response<List<Game>>
+}
