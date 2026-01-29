b09d47bcc21bfa624a0aee60d594c8b533038259
Fiercedarkvenom <db220605@gmail.com>
2026-01-28 22:47:00 -0800

    Gradle Sync

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id ("com.google.devtools.ksp")
 }
 
 android {
@@ -39,6 +40,9 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+    implementation ("androidx.room:room-runtime:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+    ksp ("androidx.room:room-compiler:2.6.1")
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
